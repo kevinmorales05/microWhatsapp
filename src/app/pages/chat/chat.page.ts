34f3884bc2a0735060b2a97f59a30f2efa5e9417
kevops <kevin.morales@jala-foundation.org>
2021-12-10 11:07:25 -0500

    First commit

---
+++ src/app/pages/chat/chat.page.ts
@@ -0,0 +1,37 @@
+import { Message } from './../../services/chat.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChatService } from '../../services/chat.service';
+import { Router } from '@angular/router';
+import { IonContent } from '@ionic/angular';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.page.html',
+  styleUrls: ['./chat.page.scss'],
+})
+export class ChatPage implements OnInit {
+  @ViewChild(IonContent) content: IonContent;
+ 
+  messages: Observable<Message[]>;
+  newMsg = '';
+  
+  constructor(private chatService: ChatService, private router: Router) { }
+
+  ngOnInit() {
+    this.messages = this.chatService.getChatMessages();
+  }
+
+  sendMessage() {
+    this.chatService.addChatMessage(this.newMsg).then(() => {
+      this.newMsg = '';
+      this.content.scrollToBottom();
+    });
+  }
+
+  signOut() {
+    this.chatService.signOut().then(() => {
+      this.router.navigateByUrl('/', { replaceUrl: true });
+    });
+  }
+}
---
+++ start.sh
@@ -0,0 +1,7 @@
+ionic start devdacticFire blank --type=angular --capacitor
+cd ./devdacticFire
+ng add @angular/fire
+ 
+ionic g page pages/login
+ionic g page pages/chat
+ionic g service services/chat
